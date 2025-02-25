f77f37a2772c404ceaac9154acc99172438511aa
Ivan_Enko <110199ws@gmail.com>
2025-02-25 18:52:58 +0200

    fix bag and return description project

--- src/components/InitProject/DescriptionProject.js
+++ src/components/InitProject/DescriptionProject.js
@@ -4,7 +4,16 @@ import "../../index.css";
 function DescriptionProject() {
   return (
     <div className="description-title-project">
-      <p>This project is an example of a website for a barbershop.</p>
+      <p>      <p>
+        This project is an example of a website for a barbershop.
+        This is a poorly designed design template, and my skills
+        are not high enough to adapt it to small screens, even computers,
+        so it is recommended to open it on a widescreen monitor for proper display.<br></br>
+        In general, this is a template from a telegram channel that was proposed in a practical assignment.
+        It was interesting to get acquainted with
+        the React framework using the example of implementing this template
+        and making a prototype of a website for a barbersop.
+      </p>.</p>
     </div>
   );
 }
