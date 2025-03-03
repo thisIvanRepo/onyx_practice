b71bdf5b1e2c99181cc5c3425d071c263ea6fc66
Ivan_Enko <110199ws@gmail.com>
2025-03-03 19:44:15 +0200

    done more section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./Main.css";
 import Hero from "../Hero/Hero";
 import History from "../History/History";
+import Services from "../Services/Services";
 
 function Main() {
   return (
     <>
       <Hero />
-      <History />
+      <History
+        modalTitle="Barbershop History" 
+        modalContent="Oh, that's all! It's a very long story..." 
+      />
+      <Services />
     </>
   );
 }
