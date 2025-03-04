de34c87c499474e1b45a8454789cee0051d85397
Ivan_Enko <110199ws@gmail.com>
2025-03-04 18:22:59 +0200

    add description of project

--- src/components/InitProject/DescriptionProject.jsx
+++ src/components/InitProject/DescriptionProject.jsx
@@ -5,14 +5,12 @@ function DescriptionProject() {
   return (
     <div className="description-title-project">
       <p>
-        This project is an example of a website for a barbershop. This is a
-        poorly designed design template, and my skills are not high enough to
-        adapt it to small screens, even computers, so it is recommended to open
-        it on a widescreen monitor for proper display.<br></br>
-        In general, this is a template from a telegram channel that was proposed
-        in a practical assignment. It was interesting to get acquainted with the
-        React framework using the example of implementing this template and
-        making a prototype of a website for a barbersop.
+        This project is an example of a website for a hair salon. This is a poorly designed design template, in my opinion :)
+        To adapt it to smaller screens than indicated in the template, I deviated from the standard styles.
+        In general, this is a template from a telegram channel, which was proposed in a practical task.
+        During the implementation, it was interesting to get acquainted with the React framework using the example
+        of implementing this template and creating a prototype of a website for a hair salon, with certain
+        interactive interaction events, I got acquainted with the work of several hooks and syntax features.
       </p>
     </div>
   );
